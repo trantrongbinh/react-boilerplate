a851b1cb66fc6a4e037b67443967e682e261a8ec
trantrongbinh <tranbinhbak@gmail.com>
2019-08-04 22:13:39 +0700

    Config feathers for server

--- server/logger.js
+++ server/logger.js
@@ -1,38 +1,11 @@
-/* eslint-disable no-console */
+const { createLogger, format, transports } = require('winston');
 
-const chalk = require('chalk');
-const ip = require('ip');
-
-const divider = chalk.gray('\n-----------------------------------');
-
-/**
- * Logger middleware, you can customize it to make messages more personal
- */
-const logger = {
-  // Called whenever there's an error on the server we want to print
-  error: err => {
-    console.error(chalk.red(err));
-  },
-
-  // Called when express.js app starts on given port w/o errors
-  appStarted: (port, host, tunnelStarted) => {
-    console.log(`Server started ! ${chalk.green('✓')}`);
-
-    // If the tunnel started, log that and the URL it's available at
-    if (tunnelStarted) {
-      console.log(`Tunnel initialised ${chalk.green('✓')}`);
-    }
-
-    console.log(`
-${chalk.bold('Access URLs:')}${divider}
-Localhost: ${chalk.magenta(`http://${host}:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
-        (tunnelStarted
-          ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}`
-          : '')}${divider}
-${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
-    `);
-  },
-};
+// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
+const logger = createLogger({
+  // To see more detailed errors, change this to 'debug'
+  level: 'info',
+  format: format.combine(format.splat(), format.simple()),
+  transports: [new transports.Console()],
+});
 
 module.exports = logger;
